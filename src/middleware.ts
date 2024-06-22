c216dab1bf1edbc423d03cb153a84f123acdb6b2
Joonho Kim <lippoint.surf0622@gmail.com>
2024-06-22 22:18:53 +0900

    change middleware original code

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,55 +2,33 @@ import { NextRequest, NextResponse } from 'next/server';
 import { isValidPassword } from './lib/isValidPassword';
 
 export async function middleware(req: NextRequest) {
-  const start = Date.now();
-
-  if (!(await isAuthenticated(req))) {
-    console.log('Authentication failed');
+  if ((await isAuthenticated(req)) === false) {
     return new NextResponse('Unauthorized', {
       status: 401,
       headers: { 'WWW-Authenticate': 'Basic' },
     });
   }
-
-  const duration = Date.now() - start;
-  console.log(`Middleware executed in ${duration}ms`);
-
-  return NextResponse.next();
 }
 
 async function isAuthenticated(req: NextRequest) {
-  const authHeader = req.headers.get('authorization');
+  const authHeader =
+    req.headers.get('authorization') || req.headers.get('Authorization');
 
-  if (!authHeader || !authHeader.startsWith('Basic ')) {
-    console.log('Authorization header is missing or invalid format');
-    return false;
-  }
+  if (authHeader == null) return false;
 
-  try {
-    const base64Credentials = authHeader.split(' ')[1];
-    const credentials = Buffer.from(base64Credentials, 'base64').toString(
-      'ascii'
-    );
-    const [username, password] = credentials.split(':');
+  const [username, password] = Buffer.from(authHeader.split(' ')[1], 'base64')
+    .toString()
+    .split(':');
 
-    const isAuthenticated =
-      username === process.env.ADMIN_USERNAME &&
-      (await isValidPassword(
-        password,
-        process.env.HASHED_ADMIN_PASSWORD as string
-      ));
-
-    if (!isAuthenticated) {
-      console.log('Invalid username or password');
-    }
-
-    return isAuthenticated;
-  } catch (error) {
-    console.error('Error parsing authentication header:', error);
-    return false;
-  }
+  return (
+    username === process.env.ADMIN_USERNAME &&
+    (await isValidPassword(
+      password,
+      process.env.HASHED_ADMIN_PASSWORD as string
+    ))
+  );
 }
 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: '/admin/:path*',
 };
